bc46e3d6c2b1059a0abc6c2aa637b1fe1359e461
Kfkcome <killzion666@gmail.com>
2023-06-23 22:00:30 +0800

    feat(domain ): 修复单位问题
    修改处理单位问题
    
    Closes 定时任务删除缓存
    
    [skip ci]

--- src/main/java/com/feidian/ChromosView/domain/LoopPointMB.java
+++ src/main/java/com/feidian/ChromosView/domain/LoopPointMB.java
@@ -10,18 +10,17 @@ import lombok.NoArgsConstructor;
 public class LoopPointMB {
     int IC_ID;
     int CS_ID;
-    long START_POINT;
-    long END_POINT;
+    Double START_POINT;
+    Double END_POINT;
     int IC_NUM;
     double FDR_VALUE;
 
     public LoopPointMB(LoopPoint loopPoint) {
-        this.END_POINT = (loopPoint.END_POINT1 + loopPoint.END_POINT2) / 2;
-        this.START_POINT = (loopPoint.START_POINT1 + loopPoint.getSTART_POINT2()) / 2;
+        this.END_POINT = (loopPoint.END_POINT1 + loopPoint.END_POINT2) / 2.0 / 10000.0;
+        this.START_POINT = (loopPoint.START_POINT1 + loopPoint.getSTART_POINT2()) / 2.0 / 10000.0;
         this.IC_ID = loopPoint.getCS_ID();
         this.IC_NUM = loopPoint.getIC_NUM();
         this.CS_ID = loopPoint.CS_ID;
         this.FDR_VALUE = loopPoint.getFDR_VALUE();
     }
-
 }
