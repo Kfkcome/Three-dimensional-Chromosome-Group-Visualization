5d85207339f400061255a13757bb77619ec039de
Kfkcome <killzion666@gmail.com>
2024-04-08 16:58:41 +0800

    fix(controller service domain): 清楚之前版本的残留

--- src/main/java/com/feidian/ChromosView/service/impl/HicServiceImpl.java
+++ src/main/java/com/feidian/ChromosView/service/impl/HicServiceImpl.java
@@ -1,14 +1,13 @@
 package com.feidian.ChromosView.service.impl;
 
 import GenerateMyHeatMap.GenerateHeatmap;
-import com.feidian.ChromosView.domain.*;
+import com.feidian.ChromosView.domain.LastQuery;
+import com.feidian.ChromosView.domain.MatrixPoint;
 import com.feidian.ChromosView.exception.HicFileNotFoundException;
-import com.feidian.ChromosView.exception.QueryException;
 import com.feidian.ChromosView.mapper.ChromosomeMapper;
 import com.feidian.ChromosView.mapper.CultivarMapper;
 import com.feidian.ChromosView.mapper.SpeciesMapper;
 import com.feidian.ChromosView.service.HicService;
-import com.feidian.ChromosView.utils.ReadFile;
 import com.feidian.ChromosView.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,10 +15,12 @@ import org.springframework.stereotype.Service;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
@@ -81,78 +82,6 @@ public class HicServiceImpl implements HicService {
         redisUtil.deleteObject(uuid + "last");
     }
 
-    @Override
-    public UUID_matrixPoints findByCS_ID(String uuid, int cs_id1, int cs_id2, String norms, String binXStart, String binYStart, String binXEnd, String binYEnd, String resolution, Integer tissue_id) throws QueryException, FileNotFoundException {
-        LastQuery nowQuery = new LastQuery(cs_id1, cs_id2, norms, binXStart, binYStart, binXEnd, binYEnd, resolution);
-        LastQuery lastQuery = redisUtil.getCacheObject(uuid + "last:");
-        List<MatrixPoint> object = redisUtil.getCacheList(uuid + "cache:");
-        Integer index = redisUtil.getCacheObject(uuid + "index:");
-        //从缓存中读取数据
-        if (object != null && nowQuery.equals(lastQuery)) {
-            System.out.println("分页查询 当前页数：" + index + "总共页数： " + object.size() / 5000);
-            int last = 5000 * (index + 1) - 1;
-            int front = 5000 * index;
-            if (front >= object.size()) {
-                removeFromCache(uuid);
-                throw new QueryException("已经到达最大索引");
-            } else if (last >= object.size()) {
-                last = object.size() - 1;
-            }
-            redisUtil.setCacheObject(uuid + "index:", index + 1);
-            return new UUID_matrixPoints(object.subList(front, last), index, object.size() / 5000, uuid);
-        }
-        if (uuid == null || uuid.isEmpty()) {
-            uuid = String.valueOf(UUID.randomUUID());
-        } else {
-            throw new QueryException("uuid错误，无法查询到数据");
-        }
-        //直接从文件中解析
-        System.out.println("直接查询");
-        ChromosomeT byCSId = chromosomeMapper.findByCS_ID(cs_id1);
-        ChromosomeT byCSId2 = chromosomeMapper.findByCS_ID(cs_id2);
-        String CSName1 = byCSId.getCS_NAME();
-        byCSId.getCULTIVAR_ID();
-        byCSId2.getCULTIVAR_ID();
-        String CSName2 = byCSId2.getCS_NAME();
-        Cultivar cultivar = cultivarMapper.findOneByTissueId(byCSId.getCULTIVAR_ID());
-        Species species = speciesMapper.findById(cultivar.getSPECIES_ID());
-        Tissue tissue = cultivarMapper.findTissueByID(tissue_id);
-        System.out.println("要查找的染色体的名字" + CSName1 + " " + CSName2);
-
-        Long binXS, binXE, binYS, binYE;
-        String file_name = species.getSPECIES_NAME() + "_" + cultivar.getCULTIVAR_NAME() + "_" + tissue.getTISSUE_NAME();
-        File file = new File("./" + file_name + "/" + file_name + ".hic");
-        if (!file.exists()) {
-            throw new FileNotFoundException(file.getPath());
-        }
-        List<MatrixPoint> matrixPoints = new ArrayList<>();
-        if (norms == null || norms.equals("NONE"))
-            if (!checkNorms(norms))
-                return new UUID_matrixPoints(matrixPoints, index, 0, uuid);
-        if (binXStart != null && binYStart != null && binXEnd != null && binYEnd != null) {
-            try {
-                binXS = Long.parseLong(binXStart);
-                binXE = Long.parseLong(binXEnd);
-                binYS = Long.parseLong(binYStart);
-                binYE = Long.parseLong(binYEnd);
-            } catch (NumberFormatException numberFormatException) {
-                matrixPoints = ReadFile.readHICALL(file, CSName1, CSName2, norms, resolution);
-                if (matrixPoints.size() >= 5000) {
-                    addToCache(uuid, nowQuery, matrixPoints);
-                    return new UUID_matrixPoints(matrixPoints.subList(0, 4999), 0, matrixPoints.size() / 5000, uuid);
-                } else return new UUID_matrixPoints(matrixPoints, 0, 0, "none");
-            }
-            matrixPoints = ReadFile.readHICByStart_End(file, CSName1, CSName2, norms, binXS, binYS, binXE, binYE, resolution);
-        } else {
-            matrixPoints = ReadFile.readHICALL(file, CSName1, CSName2, norms, resolution);
-        }
-        addToCache(uuid, nowQuery, matrixPoints);
-        //TODO:不够5000条的判断
-        if (matrixPoints.size() >= 5000) {
-            addToCache(uuid, nowQuery, matrixPoints);
-            return new UUID_matrixPoints(matrixPoints.subList(0, 4999), 0, matrixPoints.size() / 5000, uuid);
-        } else return new UUID_matrixPoints(matrixPoints, 0, 0, "none");
-    }
 
     @Override
     public String getPoint(String species, String cultivar, String tissue, String chromosome, int x, int y) throws HicFileNotFoundException {
